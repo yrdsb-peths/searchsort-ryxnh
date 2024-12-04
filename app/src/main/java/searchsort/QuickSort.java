6f1cca7f03475d87092a485fea4f254e79cd4d04
ryxnh <349739326@gapps.yrdsb.ca>
2024-12-04 10:27:24 -0500

    quick sort wip

--- app/src/main/java/searchsort/QuickSort.java
+++ app/src/main/java/searchsort/QuickSort.java
@@ -20,9 +20,34 @@ public class QuickSort extends Sort {
      * Partition the subarray arr[low..high] using the last element as pivot.
      * Return the index of the pivot element after partition.
      */
-    private int partition(int[] arr, int low, int high) {
-        return 0;
+    private int partition(int[] arr, int low, int high)
+    {
+        int i = low;
+        int j = high + 1;
+        while(true)
+        {
+            while(arr[i++] < arr[low])
+            {
+                if(i == high)
+                {
+                    break; 
+                }
+            }
+            while(arr[j--] > arr[low])
+            {
+                if(j == low)
+                {
+                    break; 
+                }
+            }
+            if(i >= j)
+            {
+                break; 
+            }
 
+        }
+
+        return j;
     }
 
 }
