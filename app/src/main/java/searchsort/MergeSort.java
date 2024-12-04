f2e36920cb48af2b1fc801e783593f2e0b52f4e3
ryxnh <349739326@gapps.yrdsb.ca>
2024-12-04 10:23:56 -0500

    merge sort

--- app/src/main/java/searchsort/MergeSort.java
+++ app/src/main/java/searchsort/MergeSort.java
@@ -6,13 +6,62 @@ public class MergeSort extends Sort {
      */
     @Override
     public void sort(int[] arr) {
+        if(arr.length <= 1)
+        {
+            return;
+        }
 
+        int mid = arr.length /  2;
+        int[]left = new int[mid]; 
+        int[]right = new int[arr.length - mid];
+
+        for(int i = 0; i < mid; i++)
+        {
+            left[i] = arr[i]; 
+        }
+
+        for(int i = mid; i < arr.length; i++)
+        {
+            right[i - mid] = arr[i];
+        }
+
+        sort(left); 
+        sort(right);
+
+        merge(arr, left, right); 
     }
 
     /*
      * Merge the two sorted arrays left and right into the array arr.
      */
-    private void merge(int[] arr, int[] left, int[] right) {
+    private void merge(int[] arr, int[] left, int[] right)
+    {
+        int i = 0;
+        int j = 0;
+        int k = 0; 
+    
 
+        while(i < left.length && j < right.length)
+        {
+            if(left[i] <= right[j])
+            {
+                arr[k++] = left[i++];
+            }
+            else
+            {
+                arr[k++] = right[j++];
+            }
+        }
+
+        while(i < left.length)
+        {
+            arr[k++] = left[i++];
+        }
+
+        while(j < right.length)
+        {
+            arr[k++] = right[j++];
+        }
+        
     }
 }
