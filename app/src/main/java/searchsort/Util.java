4ba8323fa795611524aac73f612c9e9fd1a71964
ryxnh <349739326@gapps.yrdsb.ca>
2024-12-09 09:45:12 -0500

    util wip

--- app/src/main/java/searchsort/Util.java
+++ app/src/main/java/searchsort/Util.java
@@ -47,6 +47,13 @@ public class Util {
      * Check if the array arr is sorted in ascending order.
      */
     public static boolean isSorted(int[] arr) {
+        for(int i = 1; i < arr.length; i++)
+        {
+            if(arr[i] < arr[i - 1])
+            {
+                return false;
+            }
+        }
         return true;
     }
 }
