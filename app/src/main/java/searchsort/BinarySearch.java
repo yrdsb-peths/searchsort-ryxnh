3022caa37b89c30c323b1d68fd4889be35d91113
ryxnh <349739326@gapps.yrdsb.ca>
2024-12-09 21:06:32 -0500

    binary wip

--- app/src/main/java/searchsort/BinarySearch.java
+++ app/src/main/java/searchsort/BinarySearch.java
@@ -5,9 +5,59 @@ public class BinarySearch extends Search {
      * Find the index of the target element in the sorted array arr using binary
      * search. Return -1 if target is not found in arr.
      */
+
     @Override
-    public int find(int[] arr, int target) {
-        return 0;
+    public int recursiveFind(int[] arr, int target)
+    {
+        return recursiveFind(arr, target, 0, arr.length - 1);
     }
 
+    public int recursiveFind(int[] arr, int target, int low, int high)
+    {
+        if (low > high) {
+            return -1; 
+        }
+    
+        int mid = (low + high) / 2; 
+        int cur = arr[mid];
+    
+        if (cur == target)
+        {
+            return mid; 
+        } else if (cur < target)
+        {
+            return recursiveFind(arr, target, mid + 1, high);
+        } else
+        {
+            return recursiveFind(arr, target, low, mid - 1); 
+        }
+    }
+
+    @Override
+    public int find(int[] arr, int target)
+    {
+        int low = 0; 
+        int high = arr.length - 1;
+
+        while(low <= high)
+        {
+            int mid = (low + high) / 2; 
+            int cur = arr[mid];
+            
+            if(cur == target) 
+            {
+                return mid; 
+            }
+            else if(cur < target)
+            {
+                low = mid + 1; 
+            }
+            else
+            {
+                high = mid -1; 
+            }
+        }
+
+        return -1;
+    }
 }
